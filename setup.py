a18faf144150bc55988078f3dd8da6258fd737b7
JordanChesley <jordan.r.chesley@gmail.com>
2020-02-21 00:23:38 -0500

    Updated configuration.

--- setup.py
+++ setup.py
@@ -2,14 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
   name='custconsole',
-  version='0.0.4',
+  version='0.0.5',
   description='Create a customizable console(s) for your program.',
   author='Jordan Chesley',
   author_email='jordan.r.chesley@gmail.com',
+  url='https://github.com/JordanChesley/custconsole',
   license='MIT',
   classifiers=[
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7'
   ],
   packages=find_packages(),
