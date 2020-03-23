57f5f5c71e2c9732089891a65eaa06a61c6268c6
JordanChesley <jordan.r.chesley@gmail.com>
2020-03-23 19:37:22 -0400

    Updated meta info.

--- custconsole/__init__.py
+++ custconsole/__init__.py
@@ -20,10 +20,6 @@ For documentation, please visit [].
 from .custconsole import *
 from .errors import *
 
-__all__ = [
-    '__title__', '__summary__', '__uri__', '__version__', '__author__',
-    '__email__', '__license__', '__copyright__',
-]
 
 
 __copyright__ = 'Copyright 2020 Jordan Chesley'
@@ -31,7 +27,7 @@ __copyright__ = 'Copyright 2020 Jordan Chesley'
 __title__ = 'CustConsole'
 __author__ = 'Jordan Chesley'
 __email__ = 'jordan.r.chesley@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.1'
 __summary__ = ('Create a customizable console(s) for your program.')
 __uri__ = 'https://github.com/JordanChesley/custconsole'
 __license__ = 'MIT License'
